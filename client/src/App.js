ce22cbfdd2a0dabf59851afb8859a9c5fa031a53
Suraj Mate <smate1@bajajauto.co.in>
2025-02-05 00:10:42 +0530

    landing page changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,71 @@
 
 import React from 'react';
 import './App.css';
-import EmployeeManagement from './components/EmployeeManagement';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Spinner from 'react-bootstrap/Spinner';
 
 function App() {
   return (
-    <div className="App">
-      <h1>HRMS - Employee Management</h1>
-      <EmployeeManagement />
-    </div>
+    <>
+    <Navbar fixed="top" bg="light" data-bs-theme="light">
+      <Container className='w-100 m-0'>
+        <Navbar.Brand href="#home"><strong style={{color: 'Purple'}}>CoreSync</strong></Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#blogs">Blogs</Nav.Link>
+          <Nav.Link href="#contacts">Contacts</Nav.Link>
+          <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+        </Nav>
+        <Spinner animation="border" variant="primary" />
+        <Spinner animation="border" variant="secondary" />
+        <Spinner animation="border" variant="success" />
+        <Spinner animation="border" variant="danger" />
+        <Spinner animation="border" variant="warning" />
+        <Spinner animation="border" variant="info" />
+        <Spinner animation="border" variant="light" />
+        <Spinner animation="border" variant="dark" />
+        <Spinner animation="grow" variant="primary" />
+        <Spinner animation="grow" variant="secondary" />
+        <Spinner animation="grow" variant="success" />
+        <Spinner animation="grow" variant="danger" />
+        <Spinner animation="grow" variant="warning" />
+        <Spinner animation="grow" variant="info" />
+        <Spinner animation="grow" variant="light" />
+        <Spinner animation="grow" variant="dark" />
+        <Nav className="ms-auto align-items-center">
+          {/* Add the image or avatar here */}
+          <Nav.Item>
+            <img
+              src="https://img.freepik.com/premium-vector/avatar-icon0002_750950-43.jpg?semt=ais_hybrid" // Replace with dynamic user image URL
+              alt="User Avatar"
+              style={{
+                width: '40px',
+                height: '40px',
+                borderRadius: '50%',
+                objectFit: 'cover',
+              }}
+            />
+          </Nav.Item>
+        </Nav>
+      </Container>
+    </Navbar>
+    </>
   );
 }
 
